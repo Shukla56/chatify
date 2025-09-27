5282742ea629961130f7a3f2bc39aba54b58e415
Shukla56 <shuklarahul219407@gmail.com>
2025-09-27 20:40:25 +0530

    signup controller addded

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -31,8 +31,14 @@ export const signup = async(req, res) => {
         });
 
         if(newUser) {
-            generateToken(newUser._id, res);
-            await newUser.save();
+          //before CR:
+           //generateToken(newUser._id, res);
+           //await newUser.save();
+           
+           //after CR:
+//persist user first ,thenissue auth cookie
+            const savedUser= await newUser.save();
+            generateToken(savedUser._id,res);
             res.status(201).json({
               _id: newUser._id,
               fullName: newUser.fullName,
