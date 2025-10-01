308dd6399aaa121b1d31f8afee763cac3d0b6bd0
Shukla56 <shuklarahul219407@gmail.com>
2025-10-01 13:31:34 +0530

    side bar functionality ipdated

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,8 +5,10 @@ import toast from 'react-hot-toast';
 export const useAuthStore = create((set) => ({
    authUser: null,
    isCheckingAuth: true,
-   isSigninUp:false,
-  isLoggingIn: false,
+   isSigningUp: false,
+   isLoggingIn: false,
+   isLoggingIn: false,
+
 
 checkAuth: async () => {
     try{
@@ -61,5 +63,14 @@ set({authUser: res.data})
       console.log("Logout error:", error);
     }
   },
-
+ updateProfile: async (data) => {
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("Error in update profile:", error);
+      toast.error(error.response.data.message);
+    }
+  },
 }));
