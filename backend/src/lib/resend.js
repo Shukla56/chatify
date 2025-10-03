5d9245ed11999bc9cfb37c431f17afd0324c8c2e
Shukla56 <shuklarahul219407@gmail.com>
2025-10-03 20:31:56 +0530

    some new updates

--- backend/src/lib/resend.js
+++ backend/src/lib/resend.js
@@ -1,13 +1,9 @@
 import { Resend } from "resend";
 import { ENV } from "./env.js";
 
-if (!ENV.RESEND_API_KEY) {
-    console.error("RESEND_API_KEY is not set in environment. Welcome emails will fail to send.");
-}
-
-export const resendClient = new Resend(ENV.RESEND_API_KEY || "");
+export const resendClient = new Resend(ENV.RESEND_API_KEY);
 
 export const sender = {
-    email: ENV.EMAIL_FROM || "no-reply@example.com",
-    name: ENV.EMAIL_FROM_NAME || "Chatify",
+  email: ENV.EMAIL_FROM,
+  name: ENV.EMAIL_FROM_NAME,
 };
