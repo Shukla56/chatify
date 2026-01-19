87d54003cc2fdba81b03d093f2dd99e335a96992
Rahul Shukla / <115574091+Shukla56@users.noreply.github.com>
2026-01-19 22:47:55 +0530

    Update server.js for deployment readiness
    Refactor server configuration for deployment and static file serving.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
@@ -8,25 +9,25 @@ import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import { app, server } from "./lib/socket.js";
 
-const PORT = process.env.PORT || ENV.PORT || 3000;
+const __dirname = path.resolve();
 
-app.use(express.json({ limit: "5mb" }));
+const PORT = ENV.PORT || 3000;
+
+app.use(express.json({ limit: "5mb" })); // req.body
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
-
-app.use(
-  cors({
-    origin: ENV.CLIENT_URL,
-    credentials: true
-  })
-);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// Health check (Render ke liye best)
-app.get("/", (req, res) => {
-  res.send("Backend is live 🚀");
-});
+// make ready for deployment
+if (ENV.NODE_ENV === "development") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
