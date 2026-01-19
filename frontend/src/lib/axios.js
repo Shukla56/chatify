d57e77711cb4f57ae14ec61899319cad34eb580d
Rahul Shukla / <115574091+Shukla56@users.noreply.github.com>
2026-01-19 22:48:55 +0530

    Update axios baseURL for development and production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,4 +1,6 @@
+import axios from "axios";
+
 export const axiosInstance = axios.create({
-  baseURL: "https://chat-app-backend-nsuk.onrender.com/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
   withCredentials: true,
 });
